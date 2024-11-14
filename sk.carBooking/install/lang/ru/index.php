ff7f156604cf851e29731cb7a8d68520f1a0848a
Alexey Sklyar <a.sklyar@semicvetic.com>
2024-11-14 15:46:42 +0700

    main: Правки текста в языковых файлах.

--- sk.carBooking/install/lang/ru/index.php
+++ sk.carBooking/install/lang/ru/index.php
@@ -1,7 +1,7 @@
 <?php
-$MESS["CAR_BOOKING_MODULE_NAME"] = "Модуль бронирования автомобилей";
+$MESS["CAR_BOOKING_MODULE_NAME"] = "Бронирование автомобилей";
 $MESS["CAR_BOOKING_MODULE_DESCRIPTION"] = "Модуль для бронирования служебных автомобилей.";
-$MESS["CAR_BOOKING_PARTNER_NAME"] = "ИП Скляр Алексей Вадимович";
+$MESS["CAR_BOOKING_PARTNER_NAME"] = "Алексей Скляр";
 $MESS["CAR_BOOKING_PARTNER_URI"] = "https://skliar-cv.com/";
 $MESS["CAR_BOOKING_INSTALL_TITLE"] = "Установка модуля бронирования автомобилей";
 $MESS["CAR_BOOKING_INSTALL_ERROR_VERSION"] = "Версия главного модуля ниже 14.00.00, обновите ядро системы.";
--- sk.carBooking/install/lang/ru/step1.php
+++ sk.carBooking/install/lang/ru/step1.php
@@ -1,5 +1,5 @@
 <?php
 $MESS["CAR_BOOKING_INSTALL_TITLE"] = "Установка модуля бронирования автомобилей";
-$MESS["CAR_BOOKING_INSTALL_TEST_DATA"] = "Хотите установить тестовые данные?";
-$MESS["CAR_BOOKING_INSTALL_TEST_DATA_OPTION"] = "Установить тестовые данные";
+$MESS["CAR_BOOKING_INSTALL_TEST_DATA"] = "Добавить в таблицы тестовые данные для проверки работы модуля?";
+$MESS["CAR_BOOKING_INSTALL_TEST_DATA_OPTION"] = "Добавить тестовые данные";
 $MESS["CAR_BOOKING_INSTALL_BUTTON"] = "Установить модуль";
